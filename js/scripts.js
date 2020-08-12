c51920509e9d25fc1d71bdde9d9a728482eda0cf
Alexandr-Bor <favorstreet21@gmail.com>
2020-08-13 01:41:58 +0300

    VERSION 1.0.4 *** 13.08.2020 1:42

--- js/scripts.js
+++ js/scripts.js
@@ -9,113 +9,119 @@ jQuery(function($) {
     }
 
     /* Scrolls down, hide, scrolls up, shows navigation bar + background */
-    let prevScrollpos = window.pageYOffset;
-    let elemMenu = document.getElementById("header_top_line");
-    window.onscroll = function() {
-        // background
-        if (prevScrollpos < 800) { elemMenu.style.background = "rgba(0,0,0,0.1)";} else { elemMenu.style.background = "rgba(0,0,0,0.6)";}
-        // hide, shows
-        let currentScrollPos = window.pageYOffset;
-        if (prevScrollpos < currentScrollPos) { elemMenu.style.top = "-120px";} else { elemMenu.style.top = "0";}
-        prevScrollpos = currentScrollPos;
-    }
+    ( function () {
+        let prevScrollpos = window.pageYOffset;
+        let elemMenu = document.getElementById("header_top_line");
+        window.onscroll = function() {
+            // background
+            if (prevScrollpos < 800) { elemMenu.style.background = "rgba(0,0,0,0.1)";} else { elemMenu.style.background = "rgba(0,0,0,0.6)";}
+            // hide, shows
+            let currentScrollPos = window.pageYOffset;
+            if (prevScrollpos < currentScrollPos) { elemMenu.style.top = "-120px";} else { elemMenu.style.top = "0";}
+            prevScrollpos = currentScrollPos;
+        }
+    })();
 
-    if ( screen.width > 782 ) {
-        $('.leader_mission_title').scroolly([
+    /* scroolly effects */
+    ( function () {
+        if ( screen.width > 782 ) {
+            $('.leader_mission_title').scroolly([
+                {
+                    from: 'con-top = vp-bottom',
+                    to: 'con-bottom = vp-top',
+                    cssFrom:{'margin-top':'-1px'},
+                    cssTo:{'margin-top':'-300px'},
+                }
+            ]);
+        } else {
+            $('.leader_mission_title').css({ 'margin-top' : '-77px', 'margin-bottom' : '100px' });
+        }
+
+        if ( screen.width > 500 ) {
+            $('.site_container .active_work_container .work_item_container-1 .content_of_item .right').scroolly([
+                {
+                    from: 'con-top = vp-bottom',
+                    to: 'con-bottom = vp-top',
+                    cssFrom:{'position':'relative', top :'-1px'},
+                    cssTo:{'position':'relative', top :'-200px'},
+                },
+            ]);
+            $('.site_container .active_work_container .work_item_container-4 .content_of_item .right .img_3').scroolly([
+                {
+                    from: 'con-top = vp-bottom',
+                    to: 'con-bottom = vp-top',
+                    cssFrom:{ top :'-1px'},
+                    cssTo:{ top :'-300px'},
+                },
+            ]);
+        }
+        $('.leader_box .img_box img').scroolly([
             {
-                from: 'con-top = vp-bottom',
-                to: 'con-bottom = vp-top',
-                cssFrom:{'margin-top':'-1px'},
-                cssTo:{'margin-top':'-300px'},
+                from: 'el-top = vp-bottom - 100px',
+                to: 'el-bottom = vp-bottom - 100px',
+                cssFrom:{ opacity :'0.1'},
+                cssTo:{ opacity :'1'}
+            },
+            {
+                from: 'el-top = vp-top + 100px',
+                to:   'el-bottom = vp-top + 100px',
+                cssFrom:{ opacity :'1'},
+                cssTo:{ opacity :'0.1'}
             }
         ]);
-    } else {
-        $('.leader_mission_title').css({ 'margin-top' : '-77px', 'margin-bottom' : '100px' });
-    }
 
-    if ( screen.width > 500 ) {
-        $('.site_container .active_work_container .work_item_container-1 .content_of_item .right').scroolly([
+
+        $('.description_box .signature').scroolly([
+            {
+                from: 'el-center = vp-center - 280',
+                to: 'el-top = vp-top',
+                cssFrom:{ position :'relative', left :'1px'},
+                cssTo:{ position :'relative', left :'500px'},
+            },
+        ]);
+
+
+        $('.site_container .active_work_container .work_item_container-2 .content_of_item .right').scroolly([
+            {
+                from: 'el-top = vp-bottom - 100px',
+                to: 'el-bottom = vp-bottom - 100px',
+                cssFrom:{ opacity :'0.1'},
+                cssTo:{ opacity :'1'}
+            },
+            {
+                from: 'el-top = vp-top + 100px',
+                to:   'el-bottom = vp-top + 100px',
+                cssFrom:{ opacity :'1'},
+                cssTo:{ opacity :'0.1'}
+            }
+        ]);
+        $('.site_container .active_work_container .work_item_container-3 .content_of_item .right').scroolly([
             {
                 from: 'con-top = vp-bottom',
                 to: 'con-bottom = vp-top',
-                cssFrom:{'position':'relative', top :'-1px'},
-                cssTo:{'position':'relative', top :'-200px'},
+                cssFrom:{ position :'relative', top :'220px'},
+                cssTo:{ position :'relative', top :'1px'},
             },
         ]);
-        $('.site_container .active_work_container .work_item_container-4 .content_of_item .right .img_3').scroolly([
+
+        $('.site_container .active_work_container .work_item_container-5 .content_of_item .right .img_2').scroolly([
             {
                 from: 'con-top = vp-bottom',
                 to: 'con-bottom = vp-top',
-                cssFrom:{ top :'-1px'},
-                cssTo:{ top :'-300px'},
+                cssFrom:{ top :'520px'},
+                cssTo:{ top:'100px'},
             },
         ]);
-    }
-    $('.leader_box .img_box img').scroolly([
-        {
-            from: 'el-top = vp-bottom - 100px',
-            to: 'el-bottom = vp-bottom - 100px',
-            cssFrom:{ opacity :'0.1'},
-            cssTo:{ opacity :'1'}
-        },
-        {
-            from: 'el-top = vp-top + 100px',
-            to:   'el-bottom = vp-top + 100px',
-            cssFrom:{ opacity :'1'},
-            cssTo:{ opacity :'0.1'}
-        }
-    ]);
+        $('.site_container .active_work_container .work_item_container-5 .content_of_item .right .img_3').scroolly([
+            {
+                from: 'con-top = vp-bottom',
+                to: 'con-bottom = vp-top',
+                cssFrom:{ position : 'absolute', top :'100px'},
+                cssTo:{ position : 'absolute', top :'1px'},
+            },
+        ]);
+    })();
 
-
-    $('.description_box .signature').scroolly([
-        {
-            from: 'el-center = vp-center - 280',
-            to: 'el-top = vp-top',
-            cssFrom:{ position :'relative', left :'1px'},
-            cssTo:{ position :'relative', left :'500px'},
-        },
-    ]);
-
-
-    $('.site_container .active_work_container .work_item_container-2 .content_of_item .right').scroolly([
-        {
-            from: 'el-top = vp-bottom - 100px',
-            to: 'el-bottom = vp-bottom - 100px',
-            cssFrom:{ opacity :'0.1'},
-            cssTo:{ opacity :'1'}
-        },
-        {
-            from: 'el-top = vp-top + 100px',
-            to:   'el-bottom = vp-top + 100px',
-            cssFrom:{ opacity :'1'},
-            cssTo:{ opacity :'0.1'}
-        }
-    ]);
-    $('.site_container .active_work_container .work_item_container-3 .content_of_item .right').scroolly([
-        {
-            from: 'con-top = vp-bottom',
-            to: 'con-bottom = vp-top',
-            cssFrom:{ position :'relative', top :'220px'},
-            cssTo:{ position :'relative', top :'1px'},
-        },
-    ]);
-
-    $('.site_container .active_work_container .work_item_container-5 .content_of_item .right .img_2').scroolly([
-        {
-            from: 'con-top = vp-bottom',
-            to: 'con-bottom = vp-top',
-            cssFrom:{ top :'520px'},
-            cssTo:{ top:'100px'},
-        },
-    ]);
-    $('.site_container .active_work_container .work_item_container-5 .content_of_item .right .img_3').scroolly([
-        {
-            from: 'con-top = vp-bottom',
-            to: 'con-bottom = vp-top',
-            cssFrom:{ position : 'absolute', top :'100px'},
-            cssTo:{ position : 'absolute', top :'1px'},
-        },
-    ]);
 
 
 
